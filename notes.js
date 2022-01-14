0d31b6d956dcb36202a77e50e4bc550119d9240b
Satyam Lohiya <lohiyasatyam@gmail.com>
2022-01-14 08:32:51 +0530

    Initial

--- notes.js
+++ notes.js
@@ -1,65 +1,65 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-import { addNote , removeNote , listNote ,readNote } from './getnotes.js';
+import { addNote, removeNote, listNote, readNote } from './getnotes.js';
 
 const call = yargs(hideBin(process.argv));
 
 call.command({
-    command:'add',
-    describe:'Adding a new note : ',
-    builder:{
-        title:{
-            describe:'Note Title',
-            demandOption:true,
-            type:'string'
+    command: 'add',
+    describe: 'Adding a new note : ',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
         },
-        body:{
-            describe:'Note Body',
-            demandOption:true,
-            type:'string'
+        body: {
+            describe: 'Note Body',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler(argv){
+    handler(argv) {
         console.log(`Title : ${argv.title}`)
         console.log(`Body : ${argv.body}`)
-        addNote(argv.title,argv.body)
+        addNote(argv.title, argv.body)
     }
 })
 
 call.command({
-    command:'remove',
-    describe:'Removing a note : ',
-    builder:{
-        title:{
-            describe:'Note Title',
-            demandOption:true,
-            type:'string'
+    command: 'remove',
+    describe: 'Removing a note : ',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler(argv){
+    handler(argv) {
         removeNote(argv.title)
     }
 })
 
 call.command({
-    command:'list',
-    describe:'Listing all notes : ',
-    handler(){
+    command: 'list',
+    describe: 'Listing all notes : ',
+    handler() {
         listNote()
     }
 })
 
 call.command({
-    command:'read',
-    describe:'Read a note : ',
-    builder:{
-        title:{
-            describe:'Note Title',
-            demandOption:true,
-            type:'string'
+    command: 'read',
+    describe: 'Read a note : ',
+    builder: {
+        title: {
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
         }
     },
-    handler(argv){
+    handler(argv) {
         readNote(argv.title)
     }
 })
